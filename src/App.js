3a32f12c8143f0245f216c5ae0e91d2c5c86b6e4
Kevin Yee <kkevinyee13@gmail.com>
2021-02-09 16:51:21 +1100

    added theming and hover state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import Popular from './components/Popular';
 import './App.css';
-import Battle from './components/Battle'
-
+import Battle from './components/Battle';
+import { ThemeProvider } from './contexts/theme';
+import Nav from './components/Nav';
 
 class App extends React.Component {
-    render() {
-        return (
-          <div className="container">
-            <Battle />            
-            {/* <Popular /> */}
-          </div>
-        );
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light'? 'dark' : 'light'
+        }));
+      }
     }
+  }
+  
+  render() {
+      return (
+        <ThemeProvider value={this.state}>
+          <div className={this.state.theme}>
+            <div className="container">
+              <Nav />
+              {/* <Battle />             */}
+              <Popular />
+            </div>
+          </div>
+        </ThemeProvider>
+      );
+  }
 }
 
 export default App;
